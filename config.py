686f7b7f34a5547390e75fa502aa2357b553543e
Gourang Kumar <65007870+gourangkumar@users.noreply.github.com>
2026-09-03 17:29:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
+from dataclasses import dataclass
 from dotenv import load_dotenv
 
 load_dotenv()
 
+@dataclass(frozen=True)
 class Config:
-    TYPESENSE_HOST = os.getenv("TYPESENSE_HOST", "localhost")
-    TYPESENSE_PORT = int(os.getenv("TYPESENSE_PORT", 8108))
-    TYPESENSE_PROTOCOL = os.getenv("TYPESENSE_PROTOCOL", "http")
-    TYPESENSE_API_KEY = os.getenv("TYPESENSE_API_KEY", "xyz123secret")
-    TYPESENSE_COLLECTION_NAME = os.getenv("TYPESENSE_COLLECTION_NAME", "materials")
-    
-    DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t")
-    PORT = int(os.getenv("PORT", 8000))
+    TYPESENSE_HOST: str = os.getenv("TYPESENSE_HOST", "localhost")
+    TYPESENSE_PORT: int = int(os.getenv("TYPESENSE_PORT", "8108"))
+    TYPESENSE_PROTOCOL: str = os.getenv("TYPESENSE_PROTOCOL", "http")
+    TYPESENSE_API_KEY: str = os.getenv("TYPESENSE_API_KEY", "xyz")
+    PORT: int = int(os.getenv("PORT", "8000"))
+    DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
 
 config = Config()
