4009df8368e602d43ba6c2d4c9281fd903c6cf94
Gourang Kumar <65007870+gourangkumar@users.noreply.github.com>
2026-09-04 11:33:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     TYPESENSE_HOST: str = os.getenv("TYPESENSE_HOST", "localhost")
     TYPESENSE_PORT: int = int(os.getenv("TYPESENSE_PORT", "8108"))
     TYPESENSE_PROTOCOL: str = os.getenv("TYPESENSE_PROTOCOL", "http")
-    TYPESENSE_API_KEY: str = os.getenv("TYPESENSE_API_KEY", "xyz")
+    TYPESENSE_API_KEY: str = os.getenv("TYPESENSE_API_KEY", "xyz123secret")
     PORT: int = int(os.getenv("PORT", "8000"))
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
 
